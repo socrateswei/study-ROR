9b5dbbca204ae09757d8d2f23f4ba58f499fcb2e
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-03-26 10:55:17 +0800

    [Route] Add route for plurks
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => "plurks#index"
+  resources :plurks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

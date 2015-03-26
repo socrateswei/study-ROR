0ec194d7a66ade59c9352888ad36fba11eab059c
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-03-26 11:07:05 +0800

    [Plurk] Add create method
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/controllers/plurks_controller.rb
+++ app/controllers/plurks_controller.rb
@@ -5,4 +5,15 @@ class PlurksController < ApplicationController
   def new
     @plurk = Plurk.new
   end
+  def create
+    @plurk = Plurk.new(plurk_params)
+    if @plurk.save
+      redirect_to plurks_path
+    else
+      render :new
+    end
+  end
+  def plurk_params
+    params.require(:plurk).permit(:name,:content)
+  end
 end

1ea369195750dac29d880217f5925544dcebbade
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-03-27 23:25:39 +0800

    Associate user, reply and plurk relationship
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/models/plurk.rb
+++ app/models/plurk.rb
@@ -1,5 +1,6 @@
 class Plurk < ActiveRecord::Base
   has_many :replies, :dependent => :destroy
+  belongs_to :user
   validates_presence_of :user_id
   validates_presence_of :content
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,6 @@
 class Reply < ActiveRecord::Base
   belongs_to :plurk
+  belongs_to :user
+  validates_presence_of :user_id
+  validates_presence_of :content
 end

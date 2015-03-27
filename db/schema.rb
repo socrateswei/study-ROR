9a581ea8f1a6830ccff57071d9e788f2bb31d3f7
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-03-27 23:24:05 +0800

    Add user_id to reply
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327085301) do
+ActiveRecord::Schema.define(version: 20150327095907) do
 
   create_table "plurks", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150327085301) do
     t.integer  "plurk_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|

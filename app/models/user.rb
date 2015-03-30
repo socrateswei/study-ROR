e8bb7dc2fa596f2e9d6143e30696d6c8900256f1
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-03-30 15:18:10 +0800

    Associate user and relationship
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :plurks, :dependent => :destroy
   has_many :replies, :dependent => :destroy
+  has_many :relationships
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id"
 end

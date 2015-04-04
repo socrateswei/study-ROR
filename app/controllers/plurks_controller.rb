ee801edf650f144725ec3157f028809fd2a16792
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-04-04 11:16:51 +0800

    Redirect to root_path instead of plurks_path
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/controllers/plurks_controller.rb
+++ app/controllers/plurks_controller.rb
@@ -13,18 +13,18 @@ class PlurksController < ApplicationController
   def create
     @plurk = current_user.plurks.new(plurk_params)
     if @plurk.save
-      redirect_to plurks_path
+      redirect_to root_path
     else
       render :new
     end
   end
   def destroy
     @plurk.destroy
-    redirect_to plurks_path
+    redirect_to root_path
   end
   def update
     @plurk.update(plurk_params)
-    redirect_to plurks_path
+    redirect_to root_path
   end
   def reply
     @reply = Reply.new
@@ -32,7 +32,7 @@ class PlurksController < ApplicationController
   def create_reply
     @reply = @plurk.replies.new(reply_params)
     if @reply.save
-      redirect_to plurks_path
+      redirect_to root_path
     else
       render :reply
     end

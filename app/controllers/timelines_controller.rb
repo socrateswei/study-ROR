399f9915a93f07800ec413b3772f8f2b97920a28
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-04-04 09:05:57 +0800

    Fixed timeline index error when user doesn't have any followed
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,11 @@
 class TimelinesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @plurks = Plurk.where(user_id: [current_user.id, current_user.followed_ids]).order(created_at: :desc)
+    if current_user.followed_ids.present?
+      @plurks = Plurk.where(user_id: [current_user.id, current_user.followed_ids]).order(created_at: :desc)
+    else
+      @plurks = current_user.plurks.order(created_at: :desc)
+    end
   end
 
 end

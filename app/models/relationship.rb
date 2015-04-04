77b979a8011698a3f8fb6d490c0287ba93c8cce3
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-04-04 09:43:19 +0800

    user_id and followed_id is necessary for relationship
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ActiveRecord::Base
   belongs_to :user
   belongs_to :followed, class_name: "User", foreign_key: :followed_id
+  validates_presence_of :user_id
+  validates_presence_of :followed_id
 end

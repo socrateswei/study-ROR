26c14a8ca2b18b8de1058043d7d4ebcbb8c6a01e
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-04-10 02:23:57 +0800

    Add callback function route
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/find_user' => 'users#find_user', as: :find_user
   post 'users/follow' => 'users#follow', as: :follow
   delete 'users/unfollow' => 'users#unfollow', as: :unfollow
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, only: [:show]
   root :to => "timelines#index"
   resources :plurks

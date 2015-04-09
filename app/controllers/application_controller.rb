f51473645a7cb23106320b6cefc6ae3a79d58a9a
Tzu-Hao Wei <socrates.wei@gmail.com>
2015-04-10 02:20:11 +0800

    Allow avatar in parameters
    Signed-off-by: Tzu-Hao Wei <socrates.wei@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :nickname
-    devise_parameter_sanitizer.for(:account_update) << :nickname
+    devise_parameter_sanitizer.for(:sign_up) << :nickname << :avatar
+    devise_parameter_sanitizer.for(:account_update) << :nickname << :avatar
   end
 end
